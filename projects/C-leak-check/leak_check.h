3550351636a262c7caa2913595441b4a0638374f
sujinsr <sujinsr@gmail.com>
2015-02-23 15:21:00 +0000

    completed first working test version

--- projects/C-leak-check/leak_check.h
+++ projects/C-leak-check/leak_check.h
@@ -1,8 +1,17 @@
 #ifndef LEAK_CHECK_H
 #define LEAK_CHECK_H
 
-#ifdef LEAK_CHECK
+
+/* prototype declaration */
+int init_leakcheck();
+void display_leakinfo();
+
+void* lc_malloc(unsigned long size);
+void lc_free(void *addr) ;
+
+
 #define malloc(size)	lc_malloc(size)
-#endif //LEAK_CHECK
+#define free(addr)      lc_free(addr)
+
 
 #endif //LEAK_CHECK_H
--- projects/C-leak-check/sample1.c
+++ projects/C-leak-check/sample1.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-//#define LEAK_CHECK
+#include "leak_check.h"
 
 int main()
 {
 
-	char *str;
-#ifdef LEAK_CHECK
-	init_leakdetector();
-#endif
+	char *str1;
+	char *str2, *str3, *str4, *str5;
+	
+	init_leakcheck();
 
-	str = malloc(100);
-	str = malloc(200);
+	str1 = malloc(100);
+	str2 = malloc(200);
+	str3 = malloc(300);
+	str4 = malloc(400);
+	str5 = malloc(500);
+	str2 = malloc(2000);
 
-	free(str);
+	free(str3);
+	
+	free(str5);
+	
+	free(str2);
+	
+	display_leakinfo();
 
 	return 0;
 }
